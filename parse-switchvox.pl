874fc5ec16f1bbb009c96da0752384c43dd6a951
Bob Nance <bob@novation-3.peace.daveramsey.com>
2015-05-15 11:15:48 -0500

    initial file submission to master

---
+++ parse-switchvox.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl -w
+#
+# this application will take the input handed to it by Apache
+# it will then look at the event that the switchvox sent to determine the path to go through
+# using the other variables, it will convert the GET that the switchvox sent it and convert it
+#    into variables to be passed in JSON format to the slack server as a POST
+#
+#
