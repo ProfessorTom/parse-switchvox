fefacc9ebd38be111280d0e4d2ffdd3ff229387d
Bob Nance <bob@novation.local>
2015-05-19 21:27:48 -0500

    concat the api into a singe string and send the channel as the second passed variable

--- parse-switchvox.pl
+++ parse-switchvox.pl
@@ -19,17 +19,20 @@ sub postdata { # call with postdata(api,channel,message)
     
     my $handle = LWP::UserAgent->new; # create a new object to reference
     
-    my $server_endpoint = "$api"."$channel"; # define the full URL to which to POST
-    #DEBUG print $server_endpoint."<br>\n";
+    my $server_endpoint = "$api"; # define the full URL to which to POST
+    #DEBUG
+    print $server_endpoint."<br>\n";
     
     # set custom HTTP request header fields
     my $request = HTTP::Request->new(POST => $server_endpoint); # object to add  data
     $request->header('content-type' => 'application/json'); # header data to define
-    #DEBUG print $request->header."<br>\n";
+    #DEBUG
+    print $request->header."<br>\n";
     
     # add POST data to HTTP request body
     my $post_data = '{' . "channel" . ':' . "$channel" . "," . "text" . ':' . "$message" . '}'; # a string to use for the POST
-    #DEBUG print $post_data."<br>\n";
+    #DEBUG
+    print $post_data."<br>\n";
     
     # actually connect to the URL and POST the data
     $request->content($post_data);
